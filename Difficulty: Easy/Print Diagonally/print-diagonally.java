2589ca7707439cd625b4232099c408f1743590a7
shreyash karpe <babankarpe09@gmail.com>
2026-04-03 21:33:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print Diagonally/print-diagonally.java
@@ -0,0 +1,28 @@
+class Solution {
+    static ArrayList<Integer> diagView(int mat[][]) {
+        int n = mat.length;
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        // Top row starting points
+        for (int col = 0; col < n; col++) {
+            int i = 0, j = col;
+            while (i < n && j >= 0) {
+                res.add(mat[i][j]);
+                i++;
+                j--;
+            }
+        }
+        
+        // Last column starting points
+        for (int row = 1; row < n; row++) {
+            int i = row, j = n - 1;
+            while (i < n && j >= 0) {
+                res.add(mat[i][j]);
+                i++;
+                j--;
+            }
+        }
+        
+        return res;
+    }
+}
