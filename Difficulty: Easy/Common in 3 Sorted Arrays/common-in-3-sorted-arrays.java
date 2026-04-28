47531a8364c98a5639c36433fcac7e58a826a704
shreyash karpe <babankarpe09@gmail.com>
2026-04-28 21:58:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> commonElements(int[] a, int[] b, int[] c) {
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        int i = 0, j = 0, k = 0;
+        
+        while (i < a.length && j < b.length && k < c.length) {
+            
+            // if all equal → common element
+            if (a[i] == b[j] && b[j] == c[k]) {
+                // avoid duplicates in result
+                if (res.size() == 0 || res.get(res.size() - 1) != a[i]) {
+                    res.add(a[i]);
+                }
+                i++; j++; k++;
+            }
+            // move pointer with smallest value
+            else if (a[i] < b[j]) {
+                i++;
+            } else if (b[j] < c[k]) {
+                j++;
+            } else {
+                k++;
+            }
+        }
+        
+        return res;
+    }
+}
