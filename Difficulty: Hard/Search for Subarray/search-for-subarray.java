fb2d5f995ce734f739396df95132afa36f976db7
shreyash karpe <babankarpe09@gmail.com>
2026-05-15 23:08:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Search for Subarray/search-for-subarray.java
@@ -0,0 +1,57 @@
+class Solution {
+    public ArrayList<Integer> search(int[] a, int[] b) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int n = a.length;
+        int m = b.length;
+
+        // LPS array for KMP
+        int[] lps = new int[m];
+        computeLPS(b, lps);
+
+        int i = 0, j = 0;
+
+        while (i < n) {
+            if (a[i] == b[j]) {
+                i++;
+                j++;
+            }
+
+            // Pattern found
+            if (j == m) {
+                ans.add(i - j);
+                j = lps[j - 1];
+            }
+            // Mismatch after j matches
+            else if (i < n && a[i] != b[j]) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private void computeLPS(int[] pat, int[] lps) {
+        int len = 0;
+        int i = 1;
+
+        while (i < pat.length) {
+            if (pat[i] == pat[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
